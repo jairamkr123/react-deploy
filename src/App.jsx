2f827bc2e30f886d762be8bf316cde95141cba73
jairam kumar <jairamk120@gmail.com>
2024-09-18 10:21:45 +0530

    boiler plate code react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+
+  
+  return(
+    <div>
+      <h1>Hello From React Boiler Plate</h1>
+      <h2 className="text-3xl">{import.meta.env.VITE_API_KEY}</h2>
+    </div>
+  )
+}
+
+
+export default App;
